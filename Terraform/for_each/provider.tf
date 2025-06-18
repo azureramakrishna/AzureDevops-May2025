0ae23147a84b3831bbdc2c2df30a549d1b4701cb
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2025-06-18 07:28:24 +0530

    Update provider.tf

--- Terraform/for_each/provider.tf
+++ Terraform/for_each/provider.tf
@@ -12,7 +12,4 @@ provider "azurerm" {
   features {}
 
   subscription_id = var.subscription_id
-  client_id       = "42605662-8454-49ea-8ac5-fe7ba6d4ac00"
-  use_oidc        = true   
-  tenant_id       = "459865f1-a8aa-450a-baec-8b47a9e5c904"
 }
